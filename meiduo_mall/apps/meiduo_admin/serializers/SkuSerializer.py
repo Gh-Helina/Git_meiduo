6d60793ee5e43cecae65fe56aa1375b048fc149a
hln <hln1369471@163.com>
2019-11-08 19:55:00 +0800

    重写create方法

--- meiduo_mall/apps/meiduo_admin/serializers/SkuSerializer.py
+++ meiduo_mall/apps/meiduo_admin/serializers/SkuSerializer.py
@@ -3,6 +3,18 @@ from rest_framework import serializers
 from apps.goods.models import SKUSpecification, SKU, GoodsCategory, SpecificationOption, SPUSpecification
 
 
+class SKUSpecificationSerializer(serializers.ModelSerializer):
+    """
+           SKU具体规格序列化器
+    """
+    spec_id = serializers.IntegerField()
+    option_id = serializers.IntegerField()
+
+    class Meta:
+        model = SKUSpecification
+        fields = ('spec_id', 'option_id')
+
+
 class SKUSerializer(serializers.ModelSerializer):
     '''
     获取sku表信息的序列化器
@@ -19,18 +31,32 @@ class SKUSerializer(serializers.ModelSerializer):
     category_id = serializers.IntegerField()
     # SKUSpecification里sku有related_name=specs,子表字段内容
     # sku作为父表嵌套返回子表specs
-    # specs = SKUSpecificationSerialzier(read_only=True, many=True)
+    # 关联sku具体规格表
+    specs = SKUSpecificationSerializer(many=True)
 
     class Meta:
         model = SKU  # SKU表中category外键关联了GoodsCategory分类表。spu外键关联了SPU商品表
         fields = '__all__'
 
+    # 重写父类保存方法完成sku表和sku具体规格表两张表
+    def create(self, validated_data):
+        # 1、获取specs数据
+        specs = validated_data.get('specs')
+        # 2、将specs数据从validated_data删除
+        del validated_data['specs']
+        # 保存sku表数据
+        sku = super().create(validated_data)
+        # 保存sku具体规格表数据
+        for spec in specs:
+            SKUSpecification.objects.create(sku=sku, spec_id=spec['spec_id'], option_id=spec['option_id'])
+
+        return sku
 
 #########三级分类序列化器#################
 class GoodsCategoryserializer(serializers.ModelSerializer):
     class Meta:
-        model=GoodsCategory
-        fields='__all__'
+        model = GoodsCategory
+        fields = '__all__'
 
 
 class SPUOptineSerializer(serializers.ModelSerializer):
@@ -40,7 +66,8 @@ class SPUOptineSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SpecificationOption
-        fields=('id','value')
+        fields = ('id', 'value')
+
 
 # 序列化器的第三种定义一个序列化器的方式返回specs字段
 class SPUSpecificationSerialzier(serializers.ModelSerializer):
@@ -54,13 +81,6 @@ class SPUSpecificationSerialzier(serializers.ModelSerializer):
     # 关联嵌套返回
     spu = serializers.StringRelatedField(read_only=True)
 
-
-
     class Meta:
         model = SPUSpecification  # SKUSpecification中sku外键关联了SKU表
         fields = '__all__'
-
-
-
-
-
